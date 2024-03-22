ff16cd6f0c59b5d7029d5a7d5afe4341c2d901fc
hbilly2002@gmail.com <hbilly2002@gmail.com>
2024-03-22 12:59:13 +0900

    nginx fix

--- backend/managesystem/urls.py
+++ backend/managesystem/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from graphene_django.views import GraphQLView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', GraphQLView.as_view(graphiql=True)),
