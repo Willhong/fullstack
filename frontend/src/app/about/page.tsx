b365f99f90c51a828ba3122406f2dd015f6cadf2
hbilly2002@gmail.com <hbilly2002@gmail.com>
2024-03-22 15:48:22 +0900

    init

--- frontend/src/app/about/page.tsx
+++ frontend/src/app/about/page.tsx
@@ -1,13 +1,23 @@
 "use client";
-import React from "react";
+import get from "@/util/util";
+import React, { useState } from "react";
 
-const page = () => {
+const Page = () => {
+  const [data, setdata] = useState("");
+
+  const fetchData = async () => {
+    const data = await get("https://jsonplaceholder.typicode.com/todos/1");
+
+    setdata(data);
+    console.log(data);
+  };
   return (
     <>
       <h1>this is about page</h1>
       <h2>this is about page 2</h2>
+      <button onClick={fetchData}>fetch data</button>
     </>
   );
 };
 
-export default page;
+export default Page;
